7fe083530a32c95ac95c8effa3b1be49fa4f321b
lokii <719220502@qq.com>
2023-11-27 00:17:39 +0800

    支持Function作为Lambda的参数啊啊啊啊啊

--- calcFuncComplexity/struct/MyLambda.py
+++ calcFuncComplexity/struct/MyLambda.py
@@ -1,17 +1,51 @@
-from sympy import Lambda
+from sympy import Lambda, sympify, Expr
 from sympy.core.function import BadArgumentsError
+from sympy.utilities.iterables import iterable
+from sympy.core.containers import Tuple
 
 
 # Extention of sympy.Lambda in order to support partial-application
+# Please ensure: Each param of lambda should be either Symbol or Function, not a complex expr
 
 
 class MyLambda(Lambda):
+    # Function param extension
+    # Basically copied from Lambda.__new__
+    def __new__(cls, signature, expr):
+        if iterable(signature) and not isinstance(signature, (tuple, Tuple)):
+            # sympy_deprecation_warning(
+            #     """
+            #     Using a non-tuple iterable as the first argument to Lambda
+            #     is deprecated. Use Lambda(tuple(args), expr) instead.
+            #     """,
+            #     deprecated_since_version="1.5",
+            #     active_deprecations_target="deprecated-non-tuple-lambda",
+            # )
+            # signature = tuple(signature)
+            assert False
+
+        sig = signature if iterable(signature) else (signature,)
+        sig = sympify(sig)
+
+        # cls._check_signature(sig)
+
+        # if len(sig) == 1 and sig[0] == expr:
+        # return S.IdentityFunction
+
+        return Expr.__new__(cls, sig, sympify(expr))
+
+    # Partial-application extension
     def __call__(self, *args):
-        paramLen = list(self.nargs)[0]
+        params = self.signature
+        paramLen = len(params)
         argLen = len(args)
 
         if argLen == paramLen:
-            return super().__call__(*args)
+            # return super().__call__(*args)
+            e = self.expr
+            for param, arg in zip(params, args):
+                e = e.replace(param, arg)
+            return e
 
         elif argLen < paramLen:
             params = super().variables
