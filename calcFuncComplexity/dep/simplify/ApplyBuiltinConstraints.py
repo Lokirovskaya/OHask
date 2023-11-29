3f3ba4ede6254e7e0ea0f34febc8e5b2d393c291
lokii <719220502@qq.com>
2023-11-29 11:15:57 +0800

    有限简化external func

--- calcFuncComplexity/dep/simplify/ApplyBuiltinConstraints.py
+++ calcFuncComplexity/dep/simplify/ApplyBuiltinConstraints.py
@@ -1,11 +1,47 @@
 from typing import List
 from ..struct.Constraint import Constraint
 from ..preprocess.SymbolMaker import makeExternalSymbol
+from ..preprocess.ZEncode import zDecode
 
 
 extSym = makeExternalSymbol()
 
 
 def applyBuiltinConstraints(constrList: List[Constraint]):
+    result = []
+    
     for constr in constrList:
-        constr.substitute(extSym, 1)
+        if constr.rhs.expr == extSym:
+            realName = getRealName(constr.lhs.name)
+            if compl := findBuiltin(realName):
+                constr.substitute(extSym, compl)
+            # Not a built-in function, remove from constraints
+            else:
+                continue
+        result.append(constr)
+    
+    constrList[:] = result
+            
+
+
+def getRealName(s):
+    assert s.startswith("T_")
+    encName = s[2:]
+    decName = zDecode(encName)
+    realName = decName.split(".")[0]
+    return realName
+
+
+builtinFuncs = {
+    ":": 1,
+    "I#": 1,
+    "C#": 1,
+    "fromInteger": 1,
+}
+
+
+def findBuiltin(s):
+    if s in builtinFuncs:
+        return builtinFuncs[s]
+    else:
+        return None
